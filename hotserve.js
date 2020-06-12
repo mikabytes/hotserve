20e09ace02f411ec9fbcb6a63066f19a6d834a4f
Mika <mika@wappia.se>
2020-06-12 12:29:13 +0800

    Add cors headers

--- hotserve.js
+++ hotserve.js
@@ -4,6 +4,15 @@ import expressWs from "express-ws"
 
 export default async function run({ mainHtml, dir, port }) {
   const app = express()
+
+  // add cors headers
+  app.use((req, res, next) => {
+    if (req.headers.origin) {
+      res.setHeader(`Access-Control-Allow-Origin`, req.headers.origin)
+    }
+    next()
+  })
+
   let sockets = []
   expressWs(app)
 
